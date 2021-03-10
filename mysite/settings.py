a4d5bb68559d158cf9f8931399b9f03cc1525424
phoenix-02 <50809139+phoenix-02@users.noreply.github.com>
2021-03-11 04:11:12 +0600

    remove slash from static urls cause it required by heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,13 +134,13 @@ LOGOUT_REDIRECT_URL = '/'
 
 STATICFILES_DIRS = [BASE_DIR / 'shop/static/']
 # STATICFILES_DIRS = [str(BASE_DIR) + '/static/'] если используется os
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = '/shop/static/'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'shop/media'
 
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = 'static/admin/'
